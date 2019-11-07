1691af46d829df20914f505e80656027df8e8080
Jannik Buschke <jannik.buschke@gmail.com>
2019-11-07 20:13:00 +0100

    add AutoSave

---
+++ src/AutoSave.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import {  useFormikContext } from 'formik';
+import debounce from 'lodash.debounce';
+
+export function AutoSave ({ delayMs }:{delayMs:number}) {
+    const ctx = useFormikContext();
+    const debouncedSubmit = React.useCallback(
+      debounce(() => {
+        if (ctx.dirty) {
+          ctx.submitForm().then(()=>{
+
+          })
+        }
+      }, delayMs),
+      [delayMs, ctx.dirty, ctx.submitForm]
+    );
+  
+    React.useEffect(() => {
+      debouncedSubmit();
+    }, [debouncedSubmit, ctx.values]);
+  };
