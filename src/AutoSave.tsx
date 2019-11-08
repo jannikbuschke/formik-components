6716d52aa10b900b7abe00d112bfabc65d1a0d86
Jannik Buschke <jannik.buschke@gmail.com>
2019-11-08 15:30:04 +0100

    add AutoSaveProvider and allow pausing auto-save

--- src/AutoSave.tsx
+++ src/AutoSave.tsx
@@ -3,22 +3,38 @@ import * as React from 'react';
 import {  useFormikContext } from 'formik';
 import debounce from 'lodash.debounce';
 
-export function AutoSave ({ delayMs }:{delayMs:number}) {
+interface IAutoSaveContext{
+  pause:(pause:boolean)=>void
+}
+
+export const AutoSaveContext = React.createContext<IAutoSaveContext>({pause:()=>{}})
+
+export function AutoSave ({ delayMs,pause }:{delayMs:number, pause: boolean}) {
     const ctx = useFormikContext();
     const debouncedSubmit = React.useCallback(
       debounce(() => {
-        if (ctx.dirty) {
+        if (ctx.dirty&&!pause) {
           ctx.submitForm().then(()=>{
 
           })
         }
       }, delayMs),
-      [delayMs, ctx.dirty, ctx.submitForm]
+      [delayMs, ctx.dirty, ctx.submitForm,pause]
     );
   
     React.useEffect(() => {
-      debouncedSubmit();
-    }, [debouncedSubmit, ctx.values]);
+      if(!pause){
+        debouncedSubmit();
+      }
+    }, [debouncedSubmit, ctx.values,pause]);
 
     return null
   };
+
+  export function AutoSaveProvider({children}:{children:React.ReactElement|React.ReactElement[]}) {
+    const [pause,setPause]=React.useState(false)
+    const value = React.useMemo(()=>({
+      pause:(value:boolean)=>setPause(value)
+    }),[pause])
+    return <AutoSaveContext.Provider value={value}>{children}<AutoSave delayMs={1000} pause={pause}/></AutoSaveContext.Provider>
+  }
